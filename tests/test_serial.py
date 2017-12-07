59942b5b00b773e016c2e4f40177f5d9781a6a12
kb1lqc <Bryce@FaradayRF.com>
2017-12-06 23:06:20 -0800

    Created faradayio empty send test
    test_serialEmptySynchronousSend() creates a slip packet around an empty
    string and sends it out using faradayio. The serial port is read with
    normal pyserial and the resulting data is compared to a pregenerated
    slip string which was not sent out over the serial port.

--- tests/test_serial.py
+++ tests/test_serial.py
@@ -4,7 +4,8 @@ import asyncio
 # import serial_asyncio
 # import pytest_asyncio
 import pytest
-# import sliplib
+import sliplib
+import faradayio
 
 
 def test_socketOne(event_loop):
@@ -16,3 +17,32 @@ def test_socketOne(event_loop):
     print("Loopback: {0}".format(res.decode("utf-8")))
 
     assert res.decode("utf-8") == testStr
+
+def test_serialEmptySynchronousSend():
+        """
+        Tests a synchronous faradayio send command with empty data. This
+        should take in data convert it to slip format and send it out to the
+        serial port.
+        """
+
+        serialPort = SerialTestClass()
+        slip = sliplib.Driver()
+        faraday = faradayio
+
+        emptyStr = ""
+        # testStr = "abcdefghijklmnopqrstuvwxyz0123456789"
+
+        # Create slip message to test against
+        slipMsg = slip.send(emptyStr.encode(encoding='utf_8'))
+
+        # Send data over Faraday
+        # Function should take in data, convert to utf-8 string, and return
+        # a slip encoded string. Function should return number of bytest sent
+        # including slip protocol overhead
+        res = faraday.send(emptyStr)
+
+        # Use serial to receive raw transmission with slip protocol
+        ret = serialPort.read(res)
+
+        # Check that the returned data from the serial port == slipMsg
+        assert slipMsg == ret
