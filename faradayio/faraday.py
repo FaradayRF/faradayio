f01af6bf642e90c1705af12dfa69ebd5188f2e02
kb1lqc <Bryce@FaradayRF.com>
2017-12-14 00:36:14 -0800

    Fixed flake8 errors in faraday.py, removed asyncio
    Removed asyncio support for now from the code and fixed up all flake8
    linting errors in faraday.py

--- faradayio/faraday.py
+++ faradayio/faraday.py
@@ -1,6 +1,5 @@
 import sliplib
-import asyncio
-import serial_asyncio
+
 
 class Faraday(object):
     """A class that enables transfer of data between computer and Faraday"""
@@ -36,64 +35,3 @@ class Faraday(object):
         # Yield each message as a generator
         for item in slipDriver.messages:
             yield item
-
-    async def handle_serialRecieve(serialPort):
-        """Handle a serial connection"""
-        req = await serialPort.receive(1024)
-        print(req)
-        serialPort.close()
-
-class FaradayInput(asyncio.Protocol):
-    def __init__(self):
-        super().__init__()
-        self._transport = None
-
-    def connection_made(self, transport):
-        self._transport = transport
-
-    def data_received(self, data):
-        self._transport.write(data)
-
-class Input(asyncio.Protocol):
-
-    def __init__(self):
-        super().__init__()
-        self._transport = None
-
-    def connection_made(self, transport):
-        self._transport = transport
-
-    def data_received(self, data):
-        self._transport.write(data)
-
-class Output(asyncio.Protocol):
-
-    def __init__(self):
-        super().__init__()
-        self._transport = None
-        self.received = []
-        self.actions = []
-        self.TEXT = b'Hello, World!\n'
-
-    def connection_made(self, transport):
-        self._transport = transport
-        self.actions.append('open')
-        transport.write(self.TEXT)
-
-    def data_received(self, data):
-        self.received.append(data)
-        if b'\n' in data:
-            self._transport.close()
-        return data
-
-    def connection_lost(self, exc):
-        self.actions.append('close')
-        self._transport.loop.stop()
-
-    def pause_writing(self):
-        self.actions.append('pause')
-        print(self._transport.get_write_buffer_size())
-
-    def resume_writing(self):
-        self.actions.append('resume')
-        print(self._transport.get_write_buffer_size())
