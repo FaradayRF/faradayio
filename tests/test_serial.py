53c672761afa132804691ba6e90b43584a1105c0
kb1lqc <Bryce@FaradayRF.com>
2017-12-14 00:25:00 -0800

    Added parameterized serial receive tests
    Used code from the parameters serial send unit test to test the receive
    function of the faraday module.

--- tests/test_serial.py
+++ tests/test_serial.py
@@ -58,6 +58,48 @@ def test_serialParamaterizedSynchSend(test_input):
     # Check that the returned data from the serial port == slipMsg
     assert ret == slipMsg
 
+@pytest.mark.parametrize("test_input", [
+    (b""),
+    (bytes(string.ascii_letters, "utf-8")),
+    (bytes(string.ascii_uppercase, "utf-8")),
+    (bytes(string.ascii_lowercase, "utf-8")),
+    (bytes(string.digits, "utf-8")),
+    (bytes(string.hexdigits, "utf-8")),
+    (bytes(string.printable, "utf-8")),
+    (bytes(string.octdigits, "utf-8")),
+    (sliplib.slip.END),
+    (sliplib.slip.END*2),
+    (sliplib.slip.ESC),
+    (sliplib.slip.ESC*2),
+    (sliplib.slip.ESC_ESC),
+    (sliplib.slip.ESC_ESC*2),
+    (sliplib.slip.ESC_END),
+    (sliplib.slip.ESC_END*2),
+    (sliplib.slip.END*4 + bytes(string.ascii_letters, "utf-8")),
+])
+def test_serialParamaterizedSynchReceive(test_input):
+    """
+    Tests a synchronous faradayio receive command with data. This
+    should read in data, convert it to slip format, and return the original
+    message
+    """
+
+    # Create class object necessary for test
+    serialPort = SerialTestClass()
+    slip = sliplib.Driver()
+    faradayRadio = faraday.Faraday(serialPort)
+
+    # Create slip message to test against
+    slipMsg = slip.send(test_input)
+
+    # Use serial to send raw transmission with slip protocol
+    res = serialPort.serialPort.write(slipMsg)
+
+    # Receive data from Faraday which yields each item it parses from slip
+    for item in faradayRadio.receive(res):
+        # Should be only one item
+        assert item == test_input
+
 def test_serialEmptySynchronousReceive():
         """
         Tests a synchronous faradayio receive command with empty data. This
